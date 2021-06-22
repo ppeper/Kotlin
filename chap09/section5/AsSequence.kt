0ea880d37d53fd3497f8a46f5a6e7602c8b7305e
DESKTOP-URF28CR\mnc5 <mnc5@naver.com>
2021-06-22 19:33:13 +0900

    2021.06.22
    코틀린 9장 추가

---
+++ chap09/section5/AsSequence.kt
@@ -0,0 +1,16 @@
+package chap09.section5
+
+fun main() {
+    val list1 = listOf(1,2,3,4,5)
+    val listDefault = list1
+        .map { println("map($it) "); it * it}
+        .filter { println("filter($it) "); it % 2 == 0 }
+    println(listDefault)
+
+    //asSequence()사용
+    val listSeq = list1.asSequence()
+        .map { println("map($it) "); it * it }
+        .filter { println("filter($it) "); it % 2 == 0 }
+        .toList()
+    println(listSeq)
+}
